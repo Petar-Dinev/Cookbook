2ff61d9163453209a86c62a18c24de5e7b9b477e
Petar-Dinev <p.dinev55@gmail.com>
2025-02-09 22:10:52 +0200

    add home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Home } from "./components/home/Home";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +9,9 @@ function App() {
   return (
     <>
       <h1>Hello world!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+export const Home = () => {
+  return (
+    <section>
+      <h2>
+        Welcome to Cookbook site. Here you can share your recepies and find some
+        new one for you.
+      </h2>
+    </section>
+  );
+};
